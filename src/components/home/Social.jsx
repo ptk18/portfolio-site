f7e00fbb813aa31b18264c1bce9edc05320bcf5d
ptk18 <phyoethikhin143@gmail.com>
2024-12-10 15:38:39 +0700

    initial commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Social = () => {
+    return(
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/phyothikhaing1829/" className="home__social-icon" target="_blank">
+                <i class="uil uil-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/ptk18" className="home__social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+
+            <a href="mailto:phyoethikhine143@gmail.com" className="home__social-icon" target="_blank">
+                <i class="uil uil-envelope-alt"></i>
+            </a>
+
+
+        </div>
+    )
+}
+
+export default Social
